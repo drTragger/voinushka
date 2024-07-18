8136cbfdb0e83f800dc6b2028e1d1b75b55342d2
Mykhailo Ponomarenko <my.ponomarenko@andersenlab.com>
2024-07-18 09:37:25 +0300

    Fixed buttons

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -161,7 +161,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="#" class="btn btn-outline-orange ml-auto">ДЕТАЛЬНІШЕ</a>
+                            <a href="{{ route('laserTag') }}" class="btn btn-outline-orange ml-auto">ДЕТАЛЬНІШЕ</a>
                         </div>
                     </div>
                 </div>
@@ -192,7 +192,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="#" class="btn btn-outline-orange ml-auto">ДЕТАЛЬНІШЕ</a>
+                            <a href="{{ route('laserTag') }}" class="btn btn-outline-orange ml-auto">ДЕТАЛЬНІШЕ</a>
                         </div>
                     </div>
                 </div>
@@ -353,9 +353,8 @@
                                 d="M5 4H9L11 9L8.5 10.5C9.57096 12.6715 11.3285 14.429 13.5 15.5L15 13L20 15V19C20 19.5304 19.7893 20.0391 19.4142 20.4142C19.0391 20.7893 18.5304 21 18 21C14.0993 20.763 10.4202 19.1065 7.65683 16.3432C4.8935 13.5798 3.23705 9.90074 3 6C3 5.46957 3.21071 4.96086 3.58579 4.58579C3.96086 4.21071 4.46957 4 5 4Z"
                                 stroke="#5B5E63" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                        <a class="link" href="tel:+380964862244">+38 (096) 486 2244</a>, <a class="link"
-                                                                                            href="tel:+380994862244">
-                            +38 (099) 486 2244</a>
+                        <a class="link" href="tel:+380964862244">+38 (096) 486 2244</a>,
+                        <a class="link" href="tel:+380994862244">+38 (099) 486 2244</a>
                     </p>
                 </div>
 
